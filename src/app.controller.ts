5132f06d69d202622766da628cdecfb4340661b2
Nam077 <Nam077.me@gmail.com>
2024-07-24 02:29:43 +0700

    add debouce to recommend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  @Get()
-  @Render('index')
-  root() {
-    return { message: 'Hello world!' };
-  }
+    constructor(private readonly appService: AppService) {}
+    @Get()
+    @Render('index')
+    root() {
+        return { message: 'Hello world!' };
+    }
+
+    @Post('auth-login.php')
+    test() {
+        return this.appService.loadJson();
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    loadJson() {
+        const data = fs.readFileSync('data.json', 'utf8');
+        return JSON.parse(data);
+    }
 }
